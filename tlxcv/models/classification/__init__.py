a636863688158c668fc6dac8daeaaeca95031bfc
zqwang-cn <zqwang.cn@gmail.com>
2023-04-06 10:58:45 +0800

    update alexnet

--- tlxcv/models/classification/__init__.py
+++ tlxcv/models/classification/__init__.py
@@ -2,3 +2,4 @@ from .vgg import VGG, vgg11, vgg13, vgg16, vgg19
 from .resnet import ResNet, resnet18, resnet34, resnet50, resnet101, resnet152, wide_resnet50_2, wide_resnet101_2
 from .resnest import resnest50_fast_1s1x64d, resnest50, resnest101, ResNeSt
 from .resnext import resnext50_32x4d, resnext50_64x4d, resnext101_32x4d, resnext101_64x4d, resnext152_32x4d, resnext152_64x4d, ResNeXt
+from .alexnet import alexnet, AlexNet
