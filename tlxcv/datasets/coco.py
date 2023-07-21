d555362f549580061d9ed16a779d4291b26cb42a
csJoax <joax@qq.com>
2023-07-21 09:50:43 +0800

    tlxcv/datasets/coco.py: add base class CocoData and modify for `image, label`

--- tlxcv/datasets/coco.py
+++ tlxcv/datasets/coco.py
@@ -1,37 +1,34 @@
 import os
+from typing import Callable, Optional
 
 from PIL import Image
 from pycocotools.coco import COCO
-from tensorlayerx.dataflow import Dataset
 from tensorlayerx.vision import load_image
 
+from .vision import VisionDataset
 
-class CocoDetection(Dataset):
+
+class CocoData(VisionDataset):
     def __init__(
-        self, root, split='train', transform=None, image_format='pil'
-    ):
-        if split == 'train':
-            ann_file = os.path.join(root, 'annotations/instances_train2017.json')
-        else:
-            ann_file = os.path.join(root, 'annotations/instances_val2017.json')
+        self,
+        root: str,
+        ann_file: str,
+        image_format='pil',
+        transforms: Optional[Callable] = None,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None
+    ) -> None:
+        super().__init__(root, transforms, transform, target_transform)
+
+        self.data_type = ann_file.split("_")[-1].split(".json")[0]
         self.coco = COCO(ann_file)
         self.root = root
-        self.transform = transform
-        self.ids = list(sorted(self.coco.imgs.keys()))
-        # clear 0 label
-        new_ids = []
-        for id in self.ids:
-            target = self._load_target(id)
-            anno = [obj for obj in target if "iscrowd" not in obj or obj["iscrowd"] == 0]
-            if len(anno) == 0:
-                continue
-            new_ids.append(id)
-        self.ids = new_ids
         self.image_format = image_format
-
+        self.ids = self.load_ids()
         print("load ids:", len(self.ids))
 
-        self.data_type = ann_file.split("instances_")[-1].split(".json")[0]
+    def load_ids(self):
+        raise NotImplementedError
 
     def _load_image(self, id: int):
         path = self.coco.loadImgs(id)[0]["file_name"]
@@ -43,42 +40,68 @@ class CocoDetection(Dataset):
     def _load_target(self, id):
         return self.coco.loadAnns(self.coco.getAnnIds(id))
 
+    def __len__(self) -> int:
+        return len(self.ids)
+
+
+class CocoDetection(CocoData):
+    def __init__(
+        self,
+        root: str,
+        split='train',
+        *args, **kwargs
+    ) -> None:
+        if split == 'train':
+            ann_file = os.path.join(root, 'annotations/instances_train2017.json')
+        else:
+            ann_file = os.path.join(root, 'annotations/instances_val2017.json')
+        super().__init__(root, ann_file, *args, **kwargs)
+
+    def load_ids(self):
+        ids = sorted(self.coco.imgs.keys())
+        new_ids = []
+        for id in ids:
+            target = self._load_target(id)
+            anno = [obj for obj in target if "iscrowd" not in obj or obj["iscrowd"] == 0]
+            if len(anno) == 0:
+                continue
+            new_ids.append(id)
+        return new_ids
+
     def __getitem__(self, index: int):
         id = self.ids[index]
         image = self._load_image(id)
         target = self._load_target(id)
         path = self.coco.loadImgs(id)[0]["file_name"]
-        data = {
+        label = {
             'image_id': id,
             'annotations': target,
             'path': os.path.join(self.root, self.data_type, path),
-            'image': image
         }
 
-        if self.transform:
-            data = self.transform(data)
-
-        return data
-
-    def __len__(self) -> int:
-        return len(self.ids)
+        if self.transforms:
+            image, label = self.transforms(image, label)
+        return image, label
 
 
-class CocoHumanPoseEstimation(Dataset):
+class CocoHumanPoseEstimation(CocoData):
     def __init__(
-        self, root, split='train', transform=None, image_format='pil'
-    ):
+        self,
+        root: str,
+        split='train',
+        image_format='pil',
+        *args, **kwargs
+    ) -> None:
         if split == 'train':
             ann_file = os.path.join(root, 'annotations/person_keypoints_train2017.json')
         else:
             ann_file = os.path.join(root, 'annotations/person_keypoints_val2017.json')
-        self.coco = COCO(ann_file)
-        self.root = root
-        self.transform = transform
-        self.ids = list(sorted(self.coco.imgs.keys()))
+        super().__init__(root, ann_file, *args, **kwargs)
 
+    def load_ids(self):
+        ids = sorted(self.coco.imgs.keys())
         new_ids = []
-        for id in self.ids:
+        for id in ids:
             target = self._load_target(id)
             if not target:
                 continue
@@ -88,23 +111,7 @@ class CocoHumanPoseEstimation(Dataset):
                 if sum(keypoints) == 0:
                     continue
                 new_ids.append((id, index))
-        self.ids = new_ids
-
-        self.image_format = image_format
-
-        print("load ids:", len(self.ids))
-
-        self.data_type = ann_file.split("person_keypoints_")[-1].split(".json")[0]
-
-    def _load_image(self, id: int):
-        path = self.coco.loadImgs(id)[0]["file_name"]
-        if self.image_format == 'opencv':
-            return load_image(os.path.join(self.root, self.data_type, path))
-        else:
-            return Image.open(os.path.join(self.root, self.data_type, path)).convert('RGB')
-
-    def _load_target(self, id):
-        return self.coco.loadAnns(self.coco.getAnnIds(id))
+        return new_ids
 
     def __getitem__(self, index: int):
         id, index = self.ids[index]
@@ -115,18 +122,12 @@ class CocoHumanPoseEstimation(Dataset):
         text += str(image.width) + " "
         text += " ".join(map(str, target["bbox"])) + " "
         text += " ".join(map(str, target["keypoints"])) + " "
-
-        data = {
+        label = {
             'image_id': id,
             'annotations': target,
             'text': text.strip(),
-            'image': image
         }
 
-        if self.transform:
-             data = self.transform(data)
-
-        return data
-
-    def __len__(self) -> int:
-        return len(self.ids)
+        if self.transforms:
+            image, label = self.transforms(image, label)
+        return image, label
