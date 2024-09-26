13bb5d1fba40386b1510d67ced7eae7e11a5fce1
zqwang-cn <zqwang.cn@gmail.com>
2024-09-26 16:55:36 +0800

    update object_detection predict

--- demo/object_detection/predict.py
+++ demo/object_detection/predict.py
@@ -15,7 +15,7 @@ from tensorlayerx.vision import load_image
 from tensorlayerx.vision.transforms import Compose
 from transforms import Normalize, Resize, post_process
 
-from tlxcv.models import Detr, YOLOv3, SSD
+from tlxcv.models import Detr, YOLOv3, SSD, ppyoloe
 from tlxcv.tasks.object_detection import ObjectDetection
 
 
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     # backbone = Detr(data_format=data_format)
     # backbone = YOLOv3(data_format=data_format)
     backbone = SSD(data_format=data_format)
+    # backbone = ppyoloe("ppyoloe_s", num_classes=80, data_format=data_format)
     model = ObjectDetection(backbone=backbone)
     model.load_weights("./demo/object_detection/model.npz")
     model.set_eval()
@@ -34,12 +35,19 @@ if __name__ == "__main__":
     transform = Compose(
         [
             Resize(size=800, max_size=1333, auto_divide=32),
+            # Resize(size=(640, 640), max_size=640),                # ppyoloe
             Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ]
     )
     image, _ = transform((image, None))
     inputs = {"images": tlx.convert_to_tensor([image]), "pixel_mask": None}
     outputs = model(inputs)
+    # ppyoloe
+    # scale_factor = tlx.convert_to_tensor((image.shape[0]/640, image.shape[1]/640))
+    # inputs = tlx.convert_to_tensor([image])
+    # outputs = model.predict(inputs, scale_factor=scale_factor)
+    # print(outputs)
+    # exit()
     # NOTE: with yolov3/ssd
     for s, l, b in zip(outputs["scores"], outputs["labels"], outputs["boxes"]):
         if s <= 0.5:
